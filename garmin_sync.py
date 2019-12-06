19ca77e2be6f28356f2e9de89a9b67a5e22e4d9f
Ubuntu <ubuntu@ip-172-26-10-186.ec2.internal>
2019-12-06 22:28:07 +0000

    Added support for fat percentage

--- garmin_sync.py
+++ garmin_sync.py
@@ -1,7 +1,10 @@
+# -*- coding: utf-8 -*-
 import time
 import io
+import sys
 import datetime
 import yaml
+import logging
 
 from garminexport.garminclient import GarminClient
 
@@ -12,7 +15,17 @@ from nokia import NokiaAuth, NokiaApi, NokiaCredentials
 
 from fit import FitEncoder_Weight
 
-upload_types = ['running']
+root = logging.getLogger()
+root.setLevel(logging.INFO)
+
+handler = logging.StreamHandler(sys.stderr)
+handler.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
+root.addHandler(handler)
+
+logging.basicConfig(filename='sync.log',level=logging.DEBUG)
+
 
 class Strava:
     def __init__(self, token):
@@ -34,7 +47,7 @@ class Strava:
 
         athlete = self._client.get_athlete()
         if self._verbose:
-            print("Connected to STRAVA as athelete \"{} {}\"".format(
+            logging.info("Connected to STRAVA as athelete \"{} {}\"".format(
                 athlete.firstname, athlete.lastname))
 
         return self._token
@@ -75,7 +88,7 @@ def strava_sync():
 
     if len(fit_files):
 
-        print('Uploading {} activities to STRAVA'.format(len(fit_files)))
+        logging.info('Uploading {} activities to STRAVA'.format(len(fit_files)))
 
         config['main']['last_sync'] = max(dates).timestamp()
 
@@ -84,18 +97,23 @@ def strava_sync():
         strava = Strava(config['strava'])
         strava_token = strava.connect()
 
+        upload_types = config['strava']['upload_types']
+
         for n, t, f in zip(names, types, fit_files):
             if t in upload_types:
-                print('Uploading {} type {}'.format(n, t))
+                logging.info('Uploading {} type {}'.format(n, t))
 
                 loader = strava.client.upload_activity(f, 'fit', n)
                 try:
                     loader.wait()
                 except ActivityUploadFailed as e:
-                    print('Failed to upload activity \"{}\" {}'
+                    logging.critical('Failed to upload activity \"{}\" {}'
                         .format(n, str(e)))
                 else:
-                    print('Uploaded activity \"{}\"'.format(n))
+                    logging.info('Uploaded activity \"{}\"'.format(n))
+
+            else:
+                logging.info('Skipped activity type \"{}\" for activity {}'.format(t, n))
 
         config['strava'] = strava_token
 
@@ -104,11 +122,12 @@ def strava_sync():
     with open('config.yml', 'w') as outfile:
         yaml.dump(config, outfile, default_flow_style=False)
 
-def nokia_sync():
+def nokia_sync(force=False):
     with open('config.yml') as c:
         config = yaml.load(c, Loader=yaml.FullLoader)
 
     n = config['nokia']
+
     creds = NokiaCredentials(n['access_token'],
                              n['token_expiry'],
                              n['token_type'],
@@ -130,10 +149,11 @@ def nokia_sync():
     config['nokia']['client_id'] = creds.client_id
     config['nokia']['consumer_secret'] = creds.consumer_secret
 
-    if config['nokia']['last_update'] != measure.date.timestamp:
+    if (config['nokia']['last_update'] != measure.date.timestamp) or force:
 
         # Sync Strava
 
+        logging.info('Syncing weight of {} with STRAVA.'.format(measure.weight))
         strava = Strava(config['strava'])
         strava_token = strava.connect()
         config['strava'] = strava_token
@@ -141,12 +161,17 @@ def nokia_sync():
 
         # Sync Garmin
 
+        logging.info('Syncing weight of {} with GARMIN.'.format(measure.weight))
         fit = FitEncoder_Weight()
         fit.write_file_info()
         fit.write_file_creator()
         fit.write_device_info(timestamp=measure.date.timestamp)
         fit.write_weight_scale(timestamp=measure.date.timestamp,
-                               weight=measure.weight)
+                               weight=measure.weight,
+                               percent_fat=measure.fat_ratio,
+                               percent_hydration=measure.hydration,
+                               bone_mass=measure.bone_mass,
+                               muscle_mass=measure.muscle_mass)
         fit.finish()
 
         with GarminClient(config['garmin']['username'],
@@ -155,9 +180,24 @@ def nokia_sync():
 
         config['nokia']['last_update'] = measure.date.timestamp
 
+    else:
+        logging.info("Weight of {} already synced.".format(measure.weight))
+
     with open('config.yml', 'w') as outfile:
         yaml.dump(config, outfile, default_flow_style=False)
 
+    return measure
+
 if __name__ == '__main__':
-    strava_sync()
-    nokia_sync()
+    logging.info("Started __main__")
+    try:
+        logging.info("Starting strava_sync()")
+        strava_sync()
+    except:
+        logging.critical("Error running strava_sync()")
+
+    try:
+        logging.info("Starting nokia_sync()")
+        nokia_sync()
+    except:
+        logging.critical("Error running nokia_sync()")
