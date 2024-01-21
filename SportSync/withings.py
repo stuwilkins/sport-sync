ad7fecf03f26fa78de067213a84b826f89ece6ff
Stuart B. Wilkins <swilkins@bnl.gov>
2024-01-21 14:39:08 -0500

    Cleanup

--- SportSync/withings.py
+++ SportSync/withings.py
@@ -1,16 +1,12 @@
-import os
-from os import path
 import pickle
 from typing import cast, Union
 from urllib import parse
 
 import arrow
 import datetime
-from oauthlib.oauth2.rfc6749.errors import MissingTokenError
-# from typing import Any, Callable, Dict, Iterable, Optional, Union, cast
 from typing_extensions import Final
 from withings_api import AuthScope, WithingsApi, WithingsAuth
-from withings_api.common import CredentialsType, get_measure_value, MeasureType, query_measure_groups
+from withings_api.common import CredentialsType, MeasureType, query_measure_groups, AuthFailedException
 
 
 DateType = Union[arrow.Arrow, datetime.date, datetime.datetime, int, str]
@@ -18,24 +14,29 @@ DateType = Union[arrow.Arrow, datetime.date, datetime.datetime, int, str]
 
 class WithingsData:
     def __init__(self, credentials_file='.credentials'):
-        self.credentials_file=credentials_file
-        self.credentials=None
-        self.api=None
-        self.client_id=None
-        self.consumer_secret=None
-        self.callback_uri=None
+        self.credentials_file = credentials_file
+        self.credentials = None
+        self.api = None
+        self.client_id = None
+        self.consumer_secret = None
+        self.callback_uri = None
 
     def save_credentials(self):
         """Save credentials to a file."""
         print("Saving credentials in:", self.credentials_file)
         with open(self.credentials_file, "wb") as file_handle:
             pickle.dump(self.credentials, file_handle)
+            pickle.dump(self.callback_uri, file_handle)
 
     def load_credentials(self):
         """Load credentials from a file."""
         print("Using credentials saved in:", self.credentials_file)
         with open(self.credentials_file, "rb") as file_handle:
             self.credentials = cast(CredentialsType, pickle.load(file_handle))
+            self.callback_uri = pickle.load(file_handle)
+
+        self.client_id = self.credentials.client_id
+        self.consumer_secret = self.credentials.consumer_secret
 
     def authenticate(self):
         """Authenticate to the Withings API"""
@@ -43,7 +44,7 @@ class WithingsData:
             api = WithingsApi(self.credentials, refresh_cb=self.save_credentials)
             try:
                 api.user_get_device()
-            except MissingTokenError:
+            except AuthFailedException:
                 self.credentials = None
                 print("Credentials in file are expired. Re-starting auth procedure...")
 
@@ -92,6 +93,13 @@ class WithingsData:
             enddate=None
         )
         groups = query_measure_groups(meas_result)
+
+        if len(groups) < 1:
+            return None
+
+        if len(groups[-1].measures) < 1:
+            return None
+
         measure = groups[-1].measures[-1]
         timestamp = groups[-1].date.int_timestamp
         val = float(measure.value * pow(10, measure.unit))
